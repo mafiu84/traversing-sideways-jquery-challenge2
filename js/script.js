d15ea636b9740c91fd0b4c4a80c11931b46caa0d
Mateusz Poplewski <matheush84@gmail.com>
2020-02-08 09:18:38 +0000

    traversing sideways // and up and down // jquery challenge2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 //waits until page is ready
 $(document).ready(function() {
- 
+    $("img").click(function() {
+        $(this).next().children("p").slideDown("slow");
+        });
 
+    }); 
 
-
-}); 
