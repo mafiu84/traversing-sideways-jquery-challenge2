07622a506fe01b32517aaa61867710fc8d0126e7
Mateusz Poplewski <matheush84@gmail.com>
2020-02-08 10:35:44 +0000

    traversing sideways jquery challenge4, nav bar button make elements disapear and all show them, combination of functions and selections

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 //waits until page is ready
 $(document).ready(function() {
+
     $("img").click(function() {
         $(this).next().children("p").slideDown("slow")
     });
+
     $(".card").click(function() {
 		$(this).toggleClass("highlight");	 
-	});
+    });
+
+    $("#select_btn").click(function() {
+		$(".card:not(.highlight)").hide();	 
+    });
+    
+    $("#all_btn").click(function(){
+		$(".card").show();	 
+    });
+    
 }); 
